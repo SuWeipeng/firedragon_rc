4454d2c82c4ffd5ce7d82fdf1adde38cbb71026f
SuWeipeng <weipeng.su@outlook.com>
2019-08-29 20:58:17 +0800

    加入通信状态指示：通信断-LED亮；通信异常-LED以5Hz频率闪烁。

--- Src/nrf_mavlink.c
+++ Src/nrf_mavlink.c
@@ -1,6 +1,14 @@
 #include "nrf_mavlink.h"
 #include "MY_NRF24.h"
 #include "usb_device.h"
+#include "led.h"
+
+#define _ABS_(x) (((x)>0) ? x : -x)
+#define GET_BIT(value, i) ((value)>>i)
+
+extern LED_STATUS led_status;
+
+static uint32_t last_timestamp;
 
 char myRxData[50];
 char myAckPayload[32] = "Ack by firedragon_rc";
@@ -9,6 +17,12 @@ vel_target vel;
 
 void update_mavlink(void)
 {
+  uint32_t timestamp = HAL_GetTick();
+  
+  if(_ABS_(timestamp - last_timestamp) > 500){
+    led_status = LED_ON;
+  } 
+  
   if(NRF24_available()) {
     NRF24_read(myRxData, 32);
     uint8_t i;
@@ -16,6 +30,9 @@ void update_mavlink(void)
     mavlink_status_t mav_status;
     for(i=0; i<32; i++) {
       if(mavlink_parse_char(0, myRxData[i], &msg_receive, &mav_status)) {
+        
+        last_timestamp = timestamp;
+        
         switch (msg_receive.msgid) {
 
         case MAVLINK_MSG_ID_SIMPLE: {
@@ -25,6 +42,12 @@ void update_mavlink(void)
           char buffer[32];
           sprintf(buffer, "%d\r\n", packet.data);
           VCPSend((uint8_t *)buffer, strlen(buffer));
+          uint8_t temp = GET_BIT(packet.data&0x80, 7);
+          if(temp == 1){
+            led_status = LED_BLINK_5Hz;
+          }else{
+            led_status = LED_OFF;
+          }
 
           mavlink_message_t msg_ack;
           mavlink_msg_velocity_pack(0, 0, &msg_ack, vel.vel_x, vel.vel_y, vel.rad_z);
--- Src/user_main.cpp
+++ Src/user_main.cpp
@@ -1,8 +1,10 @@
 #include "nrf_mavlink.h"
+#include "led.h"
 #include "RC_Channel.h"
 
 extern ADC_HandleTypeDef hadc1;
 extern vel_target vel;
+extern LED_STATUS led_status;
 
 RC_Channel* rc;
 
@@ -20,6 +22,8 @@ void loop(void)
   vel.vel_x = rc->vel_x();
   vel.vel_y = rc->vel_y();
   vel.rad_z = rc->rad_z(-1);
+  
+  update_led(&led_status, GPIOB, GPIO_PIN_11 | GPIO_PIN_12);
 }
 
 #if defined(__GNUC__) && defined(__cplusplus)
